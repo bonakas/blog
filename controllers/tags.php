8d96bdfc3c644b6c5efd69f8b6b7c41f86247296
bonakas <bonakas@gmail.com>
2013-10-27 23:36:01 +0200

    Add database dump

--- controllers/tags.php
+++ controllers/tags.php
@@ -10,9 +10,9 @@
 class tags extends Controller{
 	function index()
 	{
-		$this->tags= get_all("SELECT  tag_id, tag_name, COUNT(post_id) AS count
-			FROM post_tags NATURAL JOIN tag
-			GROUP BY tag_id"
-	}
+		$this->tags= get_all("SELECT tag_id, tag_name, COUNT(post_id) AS count
+                        FROM post_tags NATURAL JOIN tag
+                        GROUP BY tag_id");
+        }
 
 }
